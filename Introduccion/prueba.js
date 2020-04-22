36da2945e911bc334813c16769eaebc57e08e137
jilver <jdelacruz@gtl.com.pe>
2020-04-22 12:44:57 -0500

    Clase introductoria

---
+++ Introduccion/prueba.js
@@ -0,0 +1,10 @@
+const prueba = new Vue({
+    el:'#prueba1',
+    data:{
+      frutas:[
+          {nombre:'pera', cantidad:10},
+          {nombre:'manzana', cantidad:5},
+          {nombre:'uva', cantidad:0},
+      ]
+    }
+})
